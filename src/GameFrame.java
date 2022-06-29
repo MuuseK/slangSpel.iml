0ac1a9743f60151a29afb205c3cc7e0538718941
karimamuuse <70602661+karimamuuse@users.noreply.github.com>
2022-06-30 01:23:34 +0200

    Initial commit

---
+++ src/GameFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+public class GameFrame extends JFrame {
+    GameFrame(){
+
+    this.add(new GamePanel());
+    this.setTitle("Snake");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setResizable(false);
+    this.pack(); //functie die jframe ervoor zorgt dat de componenten groot genoeg zijn.
+    this.setVisible(true);
+    this.setLocationRelativeTo(null);
+
+    }
+}
---
+++ src/snakeMain.java
@@ -0,0 +1,7 @@
+public class snakeMain {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+
+
+}
